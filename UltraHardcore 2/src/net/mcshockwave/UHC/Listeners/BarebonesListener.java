4301db3bc54f0ea165cedb78d8ce4e1878b47768
Blaxcraft <golfboy2300@yahoo.com>
2014-04-05 12:25:50 -0700

    add new barebones stufffs

--- UltraHardcore 2/src/net/mcshockwave/UHC/Listeners/BarebonesListener.java
+++ UltraHardcore 2/src/net/mcshockwave/UHC/Listeners/BarebonesListener.java
@@ -6,7 +6,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
 
 import java.util.Random;
 
@@ -18,6 +21,7 @@ public class BarebonesListener implements Listener {
 		for (int a = 0; a < i; a++) {
 			e.getDrops().add(new ItemStack(Material.DIAMOND));
 		}
+		e.getDrops().add(new ItemStack(Material.GOLDEN_APPLE));
 	}
 
 	@EventHandler
@@ -26,6 +30,15 @@ public class BarebonesListener implements Listener {
 		if (b.getType() == Material.DIAMOND_ORE) {
 			b.breakNaturally(null);
 			b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.GOLD_INGOT));
+		}
+	}
+
+	@EventHandler
+	public void onCraftItem(PrepareItemCraftEvent event) {
+		Recipe r = event.getRecipe();
+		CraftingInventory ci = event.getInventory();
+		if (r.getResult().getType() == Material.ENCHANTMENT_TABLE || r.getResult().getType() == Material.ANVIL) {
+			ci.setResult(new ItemStack(Material.AIR));
 		}
 	}
 
