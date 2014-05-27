852cdd18ca22ec58b622827494d0a4a6fff84a01
Blaxcraft <golfboy2300@yahoo.com>
2014-05-27 16:12:36 -0700

    Triple Ores update

--- UltraHardcore 2/src/net/mcshockwave/UHC/Listeners/TripleListener.java
+++ UltraHardcore 2/src/net/mcshockwave/UHC/Listeners/TripleListener.java
@@ -15,10 +15,13 @@ public class TripleListener implements Listener {
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent event) {
 		Block b = event.getBlock();
+
+		int mult = Option.Ore_Multiplier.getInt();
+
 		if (b.getType() == Material.IRON_ORE) {
 			b.breakNaturally(null);
 			event.setExpToDrop(4);
-			for (int i = 0; i < Option.Ore_Multiplier.getInt(); i++) {
+			for (int i = 0; i < mult; i++) {
 				b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.IRON_INGOT));
 			}
 		}
@@ -26,21 +29,39 @@ public class TripleListener implements Listener {
 		if (b.getType() == Material.GOLD_ORE) {
 			b.breakNaturally(null);
 			event.setExpToDrop(8);
-			for (int i = 0; i < Option.Ore_Multiplier.getInt(); i++) {
+			for (int i = 0; i < mult; i++) {
 				b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.GOLD_INGOT));
 			}
 		}
 
 		if (b.getType() == Material.DIAMOND_ORE) {
-			for (int i = 0; i < Option.Ore_Multiplier.getInt() - 1; i++) {
+			for (int i = 0; i < mult - 1; i++) {
 				b.getWorld().dropItemNaturally(b.getLocation(),
 						new ItemStack(Scenarios.Barebones.isEnabled() ? Material.GOLD_INGOT : Material.DIAMOND));
 			}
 		}
 
 		if (b.getType() == Material.COAL_ORE) {
-			for (int i = 0; i < Option.Ore_Multiplier.getInt() - 1; i++) {
+			for (int i = 0; i < mult - 1; i++) {
 				b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.COAL));
+			}
+		}
+
+		if (b.getType() == Material.EMERALD_ORE) {
+			for (int i = 0; i < mult - 1; i++) {
+				b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.EMERALD));
+			}
+		}
+
+		if (b.getType() == Material.REDSTONE) {
+			for (int i = 0; i < (mult - 1) * 5; i++) {
+				b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.REDSTONE));
+			}
+		}
+
+		if (b.getType() == Material.LAPIS_ORE) {
+			for (int i = 0; i < (mult - 1) * 7; i++) {
+				b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(Material.INK_SACK, 1, (short) 4));
 			}
 		}
 	}
