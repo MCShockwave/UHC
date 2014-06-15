3dae22bd529ed509bca237c531cea574cbb69946
Blaxcraft <golfboy2300@yahoo.com>
2014-06-14 19:01:58 -0700

    fixed up hallucinations

--- UltraHardcore 2/src/net/mcshockwave/UHC/Listeners/HallucinationHandler.java
+++ UltraHardcore 2/src/net/mcshockwave/UHC/Listeners/HallucinationHandler.java
@@ -5,12 +5,18 @@ import net.mcshockwave.UHC.UltraHC;
 import net.mcshockwave.UHC.Utils.LocUtils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Effect;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class HallucinationHandler {
@@ -21,99 +27,84 @@ public class HallucinationHandler {
 	public static void onStartGame() {
 		sched = new BukkitRunnable() {
 			public void run() {
-				if (rand.nextInt(40) == 0) {
-					ArrayList<Player> alive = UltraHC.getAlive();
-					Player p = alive.get(rand.nextInt(alive.size()));
-
-					SoundEffect ef = getRandomEffect(p);
-
-					Location l = LocUtils.addRand(p.getLocation().clone(), 16, 10, 16);
-					if (ef.s == Sound.CREEPER_HISS || ef.s == Sound.ARROW_HIT) {
-						l = LocUtils.addRand(p.getLocation(), 5, 1, 5);
-					}
-					if (ef.vol > 5) {
-						l = LocUtils.addRand(p.getLocation(), 50, 25, 50).add(0, -25, 0);
-					}
-
-					if (UltraHC.nts.isTeamGame()) {
-						NumberTeam t = UltraHC.nts.getTeam(p.getName());
-						for (String op : t.getPlayers()) {
-							if (Bukkit.getPlayer(op) != null) {
-								ef.play(Bukkit.getPlayer(op), l);
-							}
-						}
-					} else {
-						ef.play(p, l);
-					}
-
-					for (Player p2 : Bukkit.getOnlinePlayers()) {
-						if (UltraHC.specs.contains(p2.getName())) {
-							ef.play(p2, l);
-						}
+				for (Player p : UltraHC.getAlive()) {
+					Hallucination hal = Hallucination.values()[rand.nextInt(Hallucination.values().length)];
+					if (rand.nextInt(hal.chance) == 0) {
+						hal.onActivate(p);
 					}
 				}
 			}
 		};
-		sched.runTaskTimer(UltraHC.ins, 100, 12);
+		sched.runTaskTimer(UltraHC.ins, 100, rand.nextInt(5) + 5);
 	}
-
-	public static ArrayList<SoundEffect> getList(Player p) {
-		ArrayList<SoundEffect> ret = new ArrayList<>();
-
-		for (SoundEffect ef : player) {
-			ret.add(ef);
-		}
-
-		if (p.getLocation().getY() < 40) {
-			for (SoundEffect ef : mobs) {
-				ret.add(ef);
-			}
-		}
-
-		return ret;
-	}
-
-	public static SoundEffect getRandomEffect(Player p) {
-		ArrayList<SoundEffect> efs = getList(p);
-
-		SoundEffect r = efs.get(rand.nextInt(efs.size()));
-
-		return r;
-	}
-
-	public static SoundEffect[]	player	= { n(Sound.EXPLODE, 10f, 1f), n(Sound.ITEM_PICKUP), n(Sound.HURT_FLESH),
-			n(Sound.ARROW_HIT), n(Sound.STEP_GRAVEL), n(Sound.STEP_STONE), n(Sound.STEP_GRASS) };
-	public static SoundEffect[]	mobs	= { n(Sound.ZOMBIE_IDLE), n(Sound.SKELETON_IDLE), n(Sound.SPIDER_IDLE),
-			n(Sound.CREEPER_HISS), n(Sound.EXPLODE, 10f, 1f), n(Sound.SHOOT_ARROW), n(Sound.ARROW_HIT),
-			n(Sound.ZOMBIE_HURT), n(Sound.SKELETON_HURT), n(Sound.SPIDER_DEATH), n(Sound.CREEPER_DEATH),
-			n(Sound.DIG_GRAVEL), n(Sound.DIG_STONE), n(Sound.ITEM_BREAK) };
 
 	public static void onEndGame() {
 		sched.cancel();
 	}
 
-	public static class SoundEffect {
-		public Sound	s;
-		public float	pit;
-		public float	vol;
+	public static enum Hallucination {
+		PLAYER_NAMES(
+			2),
+		DIGGING(
+			100);
 
-		private SoundEffect(Sound s, float vol, float pit) {
-			this.s = s;
-			this.pit = pit;
-			this.vol = vol;
+		public int	chance;
+
+		private Hallucination(int chance) {
+			this.chance = chance;
 		}
 
-		public void play(Player p, Location l) {
-			p.playSound(l, s, vol, pit);
+		public void onActivate(Player p) {
+			if (this == PLAYER_NAMES) {
+				Location sp = LocUtils.addRand(p.getLocation(), 25, 25, 25);
+				if (sp.getBlock().getType() == Material.AIR && sp.add(0, -1, 0).getBlock().getType() != Material.AIR) {
+					String[] names = {};
+					if (UltraHC.nts.isTeamGame()) {
+						NumberTeam nt = UltraHC.nts.teams.get(rand.nextInt(UltraHC.nts.teams.size()));
+						if (nt != UltraHC.nts.getTeam(p.getName())) {
+							List<String> nml = new ArrayList<>();
+							for (Player pl : nt.getOnlinePlayers()) {
+								nml.add(nt.t.getPrefix() + pl.getName() + nt.t.getSuffix());
+							}
+						}
+					} else {
+						Player c = UltraHC.getAlive().get(rand.nextInt(UltraHC.getAlive().size()));
+						if (c != null) {
+							names = new String[] { c.getName() };
+						}
+					}
+
+					for (String s : names) {
+						EntityType[] types = { EntityType.CHICKEN, EntityType.COW, EntityType.PIG, EntityType.SHEEP };
+						LivingEntity c = (LivingEntity) sp.getWorld()
+								.spawnEntity(sp, types[rand.nextInt(types.length)]);
+						c.setCustomName(s);
+						c.setCustomNameVisible(true);
+					}
+				}
+			}
+			if (this == DIGGING) {
+				if (p.getLocation().getY() < 40) {
+					final Location start = LocUtils.addRand(p.getLocation(), 10, 30, 10).add(0, 15, 0);
+					if (start.getBlock().getType() == Material.STONE) {
+						for (int y = 0; y < 30; y++) {
+							final int y2 = y;
+							Bukkit.getScheduler().runTaskLater(UltraHC.ins, new Runnable() {
+								public void run() {
+									final Block b = start.clone().add(0, -y2, 0).getBlock();
+									b.getWorld().playEffect(b.getLocation(), Effect.STEP_SOUND, Material.STONE);
+									Bukkit.getScheduler().runTaskLater(UltraHC.ins, new Runnable() {
+										public void run() {
+											float pitch = 1.2f + ((float) rand.nextInt(4) / (float) 10);
+											b.getWorld().playSound(b.getLocation(), Sound.ITEM_PICKUP, 0.8f, pitch);
+										}
+									}, 2);
+								}
+							}, 15 * y);
+						}
+					}
+				}
+			}
 		}
 	}
-
-	public static SoundEffect n(Sound s, float vol, float pit) {
-		return new SoundEffect(s, vol, pit);
-	}
-
-	public static SoundEffect n(Sound s) {
-		return new SoundEffect(s, 1, 1);
-	}
-
 }
